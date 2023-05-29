52ddd73a24dd88c5ef8ba0dd19cb41c5764c5da3
Paulo Alves <84809804+PauloSoaresAlves@users.noreply.github.com>
2023-05-29 19:30:25 -0300

    Update AppServer.py

--- AppServer.py
+++ AppServer.py
@@ -5,34 +5,46 @@ import json
 # Dicionário para armazenar informações dos clientees
 cliente_info = {}
 
-def registrar_cliente(socket_cliente, endereco_cliente):
-    cliente_info[endereco_cliente] = []
-    print("Novo cliente registrado:", endereco_cliente)
+def registrar_cliente(endereco_cliente):
+    if endereco_cliente not in cliente_info:
+        cliente_info[endereco_cliente] = []
+        print("Novo cliente registrado:", endereco_cliente)
+        return True
+    else:
+        print("Cliente já existe!")
+        return False
 
-def remover_cliente(socket_cliente, endereco_cliente):
+def remover_cliente(socket_cliente,endereco_cliente):
     if endereco_cliente in cliente_info:
+        socket_cliente.send("Desconectado".encode())
         del cliente_info[endereco_cliente]
         print("cliente removido:", endereco_cliente)
+        
 
 def handle_cliente_request(socket_cliente, endereco_cliente):
     while True:
-        request = socket_cliente.recv(1024).decode().split("|")
-        if request[0] == "3":
-            remover_cliente(socket_cliente, endereco_cliente)
-            socket_cliente.send("Conexão encerrada.".encode())
-            socket_cliente.close()
+        try:
+            request = socket_cliente.recv(1024).decode().split("|")
+            if request[0] == "2":
+                remover_cliente(socket_cliente,endereco_cliente)
+                socket_cliente.send("Conexão encerrada.".encode())
+                socket_cliente.close()
+                break
+            elif request[0] == "3":
+                socket_cliente.send(str(cliente_info).encode())
+            elif request[0] == "1":
+                json_message = json.loads(request[1])
+                lista_musicas = json_message["data"]
+                adicionar_conteudo_cliente(endereco_cliente, lista_musicas)
+        except:
+            del cliente_info[endereco_cliente]
+            print(f"Cliente removido por erro na conexão: {endereco_cliente}")
             break
-        elif request[0] == "1":
-            socket_cliente.send(str(cliente_info).encode())
-# parte que atualiza o dicionário de músicas, não sei se funciona
-        elif request[0] == "2":
-            json_message = json.loads(request[1:])
-            lista_musicas = json_message["data"]
-            adicionar_conteudo_cliente(endereco_cliente, lista_musicas)
 
 # atualiza a lista das músicas adicionando as novas músicas fornecidas pelo cliente
 def adicionar_conteudo_cliente(endereco_cliente, lista_musicas):
     cliente_info[endereco_cliente].extend(lista_musicas)
+    print("Tabela de Clientes:")
     print(cliente_info)
             
 def start_server():
@@ -44,12 +56,15 @@ def start_server():
 
     while True:
         socket_cliente, endereco_cliente = socket_servidor.accept()
-        registrar_cliente(socket_cliente, endereco_cliente)
-        socket_cliente.send("Registro bem-sucedido.".encode())
-        # Criar uma nova thread para lidar com a conexão do cliente
-        thread_cliente = threading.Thread(target=handle_cliente_request, args=(socket_cliente, endereco_cliente))
-        thread_cliente.daemon = True
-        thread_cliente.start()
+        success = registrar_cliente(endereco_cliente)
+        if(success):
+            socket_cliente.send("Registro bem-sucedido.".encode())
+            # Criar uma nova thread para lidar com a conexão do cliente
+            thread_cliente = threading.Thread(target=handle_cliente_request, args=(socket_cliente, endereco_cliente))
+            thread_cliente.daemon = True
+            thread_cliente.start()
+        else:
+            socket.close()
 
 if __name__ == '__main__':
     start_server()
