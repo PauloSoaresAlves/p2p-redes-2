534a7ee5fe858e0778937458a9f296549aab7776
Paulo Alves <84809804+PauloSoaresAlves@users.noreply.github.com>
2023-05-29 19:29:34 -0300

    Update AppClient.py

--- AppClient.py
+++ AppClient.py
@@ -1,35 +1,70 @@
-#TCP Socket Client
 import socket
+import threading
+import json
 
-def register_server(server_address):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(server_address)
+# Dicionário para armazenar informações dos clientees
+cliente_info = {}
 
-    client_socket.send("REGISTER".encode())
-    response = client_socket.recv(1024).decode()
-    print(response)
+def registrar_cliente(endereco_cliente):
+    if endereco_cliente not in cliente_info:
+        cliente_info[endereco_cliente] = []
+        print("Novo cliente registrado:", endereco_cliente)
+        return True
+    else:
+        print("Cliente já existe!")
+        return False
 
-    return client_socket
+def remover_cliente(socket_cliente,endereco_cliente):
+    if endereco_cliente in cliente_info:
+        socket_cliente.send("Desconectado".encode())
+        del cliente_info[endereco_cliente]
+        print("cliente removido:", endereco_cliente)
+        
 
-def unregister_server(client_socket):
-    client_socket.send("QUIT".encode())
-    response = client_socket.recv(1024).decode()
-    print(response)
-    client_socket.close()
+def handle_cliente_request(socket_cliente, endereco_cliente):
+    while True:
+        try:
+            request = socket_cliente.recv(1024).decode().split("|")
+            if request[0] == "2":
+                remover_cliente(socket_cliente,endereco_cliente)
+                socket_cliente.send("Conexão encerrada.".encode())
+                socket_cliente.close()
+                break
+            elif request[0] == "3":
+                socket_cliente.send(str(cliente_info).encode())
+            elif request[0] == "1":
+                json_message = json.loads(request[1])
+                lista_musicas = json_message["data"]
+                adicionar_conteudo_cliente(endereco_cliente, lista_musicas)
+        except:
+            del cliente_info[endereco_cliente]
+            print(f"Cliente removido por erro na conexão: {endereco_cliente}")
+            break
 
-def request_server_list(client_socket):
-    client_socket.send("LIST".encode())
-    response = client_socket.recv(1024).decode()
-    print("Lista de clientes conectados e seus arquivos:\n", response)
+# atualiza a lista das músicas adicionando as novas músicas fornecidas pelo cliente
+def adicionar_conteudo_cliente(endereco_cliente, lista_musicas):
+    cliente_info[endereco_cliente].extend(lista_musicas)
+    print("Tabela de Clientes:")
+    print(cliente_info)
+            
+def start_server():
+    socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_servidor.bind(('localhost', 5445))
+    socket_servidor.listen(5)
 
-if __name__ == '__main__':
-    server_address = ('localhost', 5445)
-    client_socket = register_server(server_address)
+    print("Servidor iniciado. Aguardando conexões...")
 
     while True:
-        user_input = input("Digite 'list' para solicitar a lista de clientes ou 'quit' para encerrar: ")
-        if user_input == "list":
-            request_server_list(client_socket)
-        elif user_input == "quit":
-            unregister_server(client_socket)
-            break
+        socket_cliente, endereco_cliente = socket_servidor.accept()
+        success = registrar_cliente(endereco_cliente)
+        if(success):
+            socket_cliente.send("Registro bem-sucedido.".encode())
+            # Criar uma nova thread para lidar com a conexão do cliente
+            thread_cliente = threading.Thread(target=handle_cliente_request, args=(socket_cliente, endereco_cliente))
+            thread_cliente.daemon = True
+            thread_cliente.start()
+        else:
+            socket.close()
+
+if __name__ == '__main__':
+    start_server()
